dec25137b0f6ab5b83deee505e84e994df88f44d
Kelvin Lu <klu@ucdavis.edu>
2015-01-23 14:54:28 -0800

    Added env and *.db to gitignore. Basic directory structure for flask made.

---
+++ backend/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('settingsbackend')
+db = SQLAlchemy(app)
+
+try:
+    db.create_all()
+except Exception:
+    # Already exists
+    print("Tried to create database")
+    pass
