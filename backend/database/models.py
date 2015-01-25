2b8a7c4323abc1f49027ac2a8752dc8c44f14aa4
Kelvin Lu <klu@ucdavis.edu>
2015-01-24 17:35:45 -0800

    Added comments to models

--- backend/database/models.py
+++ backend/database/models.py
@@ -4,6 +4,10 @@ import datetime
 
 
 class User(db.Model):
+    """
+    Represents a player and their account information. 
+    Instantiation of a User object should reflect an account registration.
+    """
     id =            db.Column(db.Integer, primary_key=True)
     username =      db.Column(db.String(20), unique=True)
     password =      db.Column(db.String(40))
@@ -12,6 +16,7 @@ class User(db.Model):
     last_name =     db.Column(db.String(40))
     avatar_url =    db.Column(db.String(100))
 
+    # 
     activity =      db.relationship('Presence', backref='user', lazy='dynamic', uselist=False)
     stats =         db.relationship('UserStatistics', backref='user', lazy='dynamic', uselist=False)
     wins =          db.relationship('GameStatistics', backref='user', lazy='dynamic')
@@ -32,6 +37,10 @@ class User(db.Model):
 
 
 class Presence(db.Model):
+    """
+    Represents a player's activity in the game.
+    Has a strong one-to-one mapping with a User.
+    """
     id =        db.Column(db.Integer, primary_key=True)
     online =    db.Column(db.Boolean)
     last_seen = db.Column(db.DateTime)
@@ -50,31 +59,45 @@ class Presence(db.Model):
 
 
 
+# Many-to-many table relating games that users have played.
 games = db.Table('games',
     db.Column('game_id', db.Integer, db.ForeignKey('game.id')),
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
 )
 
 class Game(db.Model):
+    """
+    Represents a record of some game played.
+    Has a many-to-many relationship with Users and a one-on-one relationship with some
+    single-game statistics.
+    """
     id =            db.Column(db.Integer, primary_key=True)
+    time_played =   db.Column(db.DateTime)
 
     stats =         db.relationship('GameStatistics', backref='game', lazy='dynamic', uselist=False)
 
 
 
 class GameStatistics(db.Model):
+    """
+    Contains statistics for a single game played.
+    Has a strong one-to-one mapping with a Game.
+    """
     id =                db.Column(db.Integer, primary_key=True)
 
-    time_played =       db.Column(db.DateTime)
     winner_user_id =    db.Column(db.Integer, db.ForeignKey('user.id'))
     game_id =           db.Column(db.Integer, db.ForeignKey('game.id'))
 
 
 
 class UserStatistics(db.Model):
+    """
+    Contains running statistics for a User's history.
+    Has a strong one-to-one mapping with a User.    
+    """
     id =                db.Column(db.Integer, primary_key=True)
-    win_loss_ratio =    db.Column(db.Float)
     games_played =      db.Column(db.Integer)
     wins =              db.Column(db.Integer)
+    win_loss_ratio =    db.Column(db.Float)
 
     user_id =   db.Column(db.Integer, db.ForeignKey('user.id'))
