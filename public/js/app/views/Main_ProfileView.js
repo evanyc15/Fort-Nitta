9c51329077c4383f53450e4f33b6b6a54145b903
Evan Chen <echen@192.168.1.21>
2015-01-27 03:09:45 -0800

    Uploading pictures is done

--- public/js/app/views/Main_ProfileView.js
+++ public/js/app/views/Main_ProfileView.js
@@ -17,7 +17,8 @@ define([
 		events: {
 			"mouseenter #profilePicture,#profilePictureChange": "changeProfilePictureShow",
 			"mouseleave #profilePicture,#profilePictureChange": "changeProfilePictureHide",
-			"click #profilePictureChange": "changeProfilePicture"
+			"click #profilePictureChange": "changeProfilePicture",
+			"change #profilePictureChangeInput": "saveFile"
 		},
 		changeProfilePictureShow: function() {
 			$("#profilePictureChange").show();
@@ -27,6 +28,30 @@ define([
 		},
 		changeProfilePicture: function() {
 			$("#profilePictureChangeInput").click();
-		}
+		},
+		saveFile: function() {
+			var picture = $('input[name="imageInput"]')[0].files[0]; 
+			var data = new FormData();
+			data.append('file', picture);
+			$.ajax({
+				url: 'http://localhost:5000/api/avatar/',
+				type: 'POST',
+				data: data,
+				cache: false,
+				contentType: false,
+				processData: false,
+				crossDomain: true,
+				xhrFields: {
+					withCredentials: true
+				},
+				success: function(data){
+					$("#profilePicture").attr('src','http://localhost:5000/api/avatar/'+ data.username)
+				},
+				error: function(data){
+					alert('no upload');
+					$('#loadingModal').modal('hide');
+				}
+	  		});
+		},
 	});
 });
