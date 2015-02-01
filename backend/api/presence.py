a0df09c31009aae3108020be9eb826821a5015ed
Evan Chen <evanchen4@gmail.com>
2015-02-01 03:35:58 -0800

    SSE is implemented and working

--- backend/api/presence.py
+++ backend/api/presence.py
@@ -1,10 +1,36 @@
-from flask import request, jsonify, Response
+from flask import request, jsonify, Response, session, redirect, make_response
 from flask.views import MethodView
 from sqlalchemy import and_, or_
 
 from backend import db, app
 from backend.database.models import Presence, User
 
+import gevent, json
+
+presence_list = []
+
+def event_stream():
+    count = 0
+    while True:
+        gevent.sleep(15)
+        
+        user_presences = Presence.query.join(Presence.user).filter(or_(Presence.game_online == True, Presence.web_online == True)).all()
+        if user_presences is None:
+            return jsonify(**{'success': False}), 401
+
+        for new_data in user_presences:
+            if not any(init_data['username'] == new_data.user.username for init_data in presence_list):
+            # if new_data.user.username not in init_data:
+                jsonData = {'username': new_data.user.username,'first_name': new_data.user.first_name, 'last_name': new_data.user.last_name, 'web_online': new_data.web_online, 'game_online': new_data.game_online}
+                presence_list.append(jsonData)
+        # return jsonify(data = presence_list)
+        return "data: %s\n\n" % json.dumps(presence_list)
+
+@app.route('/stream')
+def stream():
+    response = make_response(event_stream())
+    response.headers['Content-Type'] = "text/event-stream"
+    return response
 
 class PresenceOnlineApi(MethodView):
     def get(self):
